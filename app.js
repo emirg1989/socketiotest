a93e20af27712e7eb386890f715493fc5eabb0a0
emirganimi <emir.ganimi@cursor.cl>
2018-06-24 07:24:07 +0200

    nuevo

--- app.js
+++ app.js
@@ -1,66 +1,28 @@
-// This is the server-side file of our mobile remote controller app.
-// It initializes socket.io and a new express instance.
-// Start it by running 'node app.js' from your terminal.
-
-
-// Creating an express server
-
-var express = require('express'),
-	app = express();
-
-// This is needed if the app is run on heroku and other cloud providers:
+var express = require('express');
+var app = express();
+var stringify = require('json-stringify');
 
 var port = process.env.PORT || 8080;
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-// Initialize a new socket.io object. It is bound to 
-// the express app, which allows them to coexist.
+var messages = [{
+  id: 1,
+  text: "hola soy un mensaje",
+  author: "emir"}];
 
-var io = require('socket.io').listen(app.listen(port));
-
-
-// App Configuration
-
-// Make the files in the public folder available to the world
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 
-// This is a secret key that prevents others from opening your presentation
-// and controlling it. Change it to something that only you know.
-
-var secret = 'kittens';
-
-// Initialize a new socket.io application
-
-var presentation = io.on('connection', function (socket) {
-
-	// A new client has come online. Check the secret key and 
-	// emit a "granted" or "denied" message.
-
-	socket.on('load', function(data){
-
-		socket.emit('access', {
-			access: (data.key === secret ? "granted" : "denied")
-		});
-
-	});
-
-	// Clients send the 'slide-changed' message whenever they navigate to a new slide.
-
-	socket.on('slide-changed', function(data){
-
-		// Check the secret key again
-
-		if(data.key === secret) {
-
-			// Tell all connected clients to navigate to the new slide
-			
-			presentation.emit('navigate', {
-				hash: data.hash
-			});
-		}
-
-	});
-
+io.on('connection', function(socket){
+  console.log("Alguien se conecto con socket");
+  socket.emit('messages',messages);
+  socket.on('new-message', function(data){
+      messages.push(data);
+      io.sockets.emit('messages', messages);
+  });
 });
 
-console.log('Your presentation is running on http://localhost:' + port);
+server.listen(port, function(){
+  console.log("Servidor corriendo ok");
+});
